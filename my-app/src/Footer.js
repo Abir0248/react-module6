fa7cd6c7d0b57f7634a45c4c370c7416ea3bf4a2
Abir0248 <abir15-6789@diu.edu.bd>
2023-07-11 17:40:12 +0600

    footer

---
+++ my-app/src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>All rights reserved to Abir.</p>
+      <p>
+        "I to die, and you to live. Which is better only God knows.” ― Socrates
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
